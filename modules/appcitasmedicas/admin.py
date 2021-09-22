78afd139d3abeeca0d37770bab6a845f6c699b66
Tito Andrés Maturana <tamatu@gmail.com>
2021-09-22 07:56:58 -0500

    admin.py

--- modules/appcitasmedicas/admin.py
+++ modules/appcitasmedicas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from modules.appcitasmedicas.models import *
+admin.site.register(Pacientes)
+admin.site.register(Medicos)
+admin.site.register(Disponibilidad)
+admin.site.register(Citas)

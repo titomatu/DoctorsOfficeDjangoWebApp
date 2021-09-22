109620b946fb2b93dc2100a8e93e397193dbf085
Tito Andrés Maturana <tamatu@gmail.com>
2021-09-22 07:55:14 -0500

    Cascarón proyecto django

---
+++ modules/appcitasmedicas/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Pacientes(models.Model):
+    pass
+
+class Medicos(models.Model):
+    pass
+
+class Disponibilidad(models.Model):
+    pass
+
+class Citas(models.Model):
+    pass

e22cafefcce7d300144da6e0b9532b3e9df5dd6f
Elkin Eduardo Bedoya Muñoz <elkinbm77@gmail.com>
2021-09-23 09:29:01 -0500

    cambios en apps.py correccion de ruta

--- modules/appcitasmedicas/apps.py
+++ modules/appcitasmedicas/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppcitasmedicasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appcitasmedicas'
+    name = 'modules.appcitasmedicas'

af0c3f01e3b2c8e8c5cbf956977ba095d2446bd0
Tito Andrés Maturana <tamatu@gmail.com>
2021-09-27 11:04:58 -0500

    login-logout

--- modules/appcitasmedicas/views.py
+++ modules/appcitasmedicas/views.py
@@ -1,7 +1,41 @@
+from modules.appcitasmedicas.models import Paciente
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def home(request):
+
+    try:
+        del request.session['id_paciente']
+    except KeyError:
+        pass
+
+    return render(request, 'home.html')
+
+def autenticar(request):
+
+    #Authenticate user
+    try:
+        usuario = request.POST["username"]
+        password = request.POST["password"]
+
+        paciente = Paciente.objects.get(id=usuario, password=password)
+        print(paciente)
+        context = {"paciente" : paciente}
+        request.session['id_paciente'] = paciente.id
+    
+
+    except ObjectDoesNotExist:
+        context = {"mensaje" : "Paciente y/o contraseña inválidos."}
+        return render(request, 'home.html', context)    
+
+    return render(request, 'index.html', context)
+
+def logout(request):
+
+    try:
+        del request.session['id_paciente']
+    except KeyError:
+        pass
 
     return render(request, 'home.html')
--- prjconsultoriomedico/urls.py
+++ prjconsultoriomedico/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from modules.appcitasmedicas.views import home
+from modules.appcitasmedicas.views import autenticar, home, logout
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('autenticar/', autenticar, name='autenticar'),
+    path('logout/', logout, name='logout')
 ]
